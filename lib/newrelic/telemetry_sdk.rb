b4b85eb268fd8ab3c170ff18a613bb02be46a9c9
Rachel Klein <rklein@newrelic.com>
2020-09-10 11:53:51 -0700

    Add gem structure

---
+++ lib/newrelic/telemetry_sdk.rb
@@ -0,0 +1,8 @@
+require "newrelic/telemetry_sdk/version"
+
+module Newrelic
+  module TelemetrySdk
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/newrelic/telemetry_sdk/version.rb
@@ -0,0 +1,5 @@
+module Newrelic
+  module TelemetrySdk
+    VERSION = "0.1.0"
+  end
+end
