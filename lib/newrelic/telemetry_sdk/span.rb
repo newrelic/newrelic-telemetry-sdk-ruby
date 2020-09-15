bb90a05facaae5bcf71843e428d005311c55bf4f
Rachel Klein <rklein@newrelic.com>
2020-09-15 14:45:44 -0700

    Build hash as part of JSON transformation

--- lib/newrelic/telemetry_sdk/span.rb
+++ lib/newrelic/telemetry_sdk/span.rb
@@ -3,41 +3,62 @@
 # See https://github.com/newrelic/newrelic-telemetry-sdk-ruby/blob/main/LICENSE for complete details.
 
 require 'newrelic/telemetry_sdk/util'
+require 'json'
 
 module Newrelic
   module TelemetrySdk
     class Span
-      attr_reader :contents
+      attr_reader :name,
+                  :id,
+                  :trace_id,
+                  :timestamp_ms,
+                  :duration_ms,
+                  :parent_id,
+                  :service_name,
+                  :custom_attributes
 
       def initialize(name,
                      id: Util.generate_guid(8),
                      trace_id: Util.generate_guid(16),
-                     timestamp: Util.time_to_ms,
-                     duration: nil,
+                     timestamp_ms: Util.time_to_ms,
+                     duration_ms: nil,
                      parent_id: nil,
                      service_name: nil,
                      custom_attributes: nil)
 
-        @attributes = {
-          name: name,
-          duration: duration,
-          parent_id: parent_id,
-          service_name: service_name
-        }
-
-        @attributes.merge!(custom_attributes) if custom_attributes
-
-        @contents = {
-          id: id,
-          trace_id: trace_id,
-          timestamp: timestamp,
-          attributes: @attributes
-        }
+        @name = name
+        @id = id
+        @trace_id = trace_id
+        @timestamp_ms = timestamp_ms
+        @duration_ms = duration_ms
+        @parent_id = parent_id
+        @service_name = service_name
+        @custom_attributes = custom_attributes
       end
 
       def finish(end_time_ms: Util.time_to_ms)
-        duration = end_time_ms - @contents[:timestamp]
-        @contents[:attributes][:duration] = duration
+        @duration_ms = end_time_ms - @timestamp_ms
+      end
+
+      def to_h
+        data = {
+          :id => @id,
+          :'trace.id' => @trace_id,
+          :timestamp => @timestamp_ms,
+          :attributes => {
+            :'duration.ms' => @duration_ms,
+            :'parent.id' => @parent_id,
+            :'service.name' => @service_name
+          }
+        }
+
+        data[:attributes].merge! @custom_attributes if @custom_attributes
+
+        data
+      end
+
+      def to_json
+        to_h.to_json
       end
     end
   end
--- test/newrelic/telemetry_sdk/span_test.rb
+++ test/newrelic/telemetry_sdk/span_test.rb
@@ -12,81 +12,87 @@ module Newrelic
     class SpanTest < Minitest::Test
       def test_required_attributes
         span = Span.new("Name")
-        assert span.contents[:id].is_a? String
-        assert span.contents[:trace_id].is_a? String
-        assert span.contents[:timestamp].is_a? Integer
-
-        expected_attributes = {
-          :name=>"Name",
-          :duration=>nil,
-          :parent_id=>nil,
-          :service_name=>nil
-        }
-
-        assert_equal expected_attributes, span.contents[:attributes]
+        assert span.id.is_a? String
+        assert span.trace_id.is_a? String
+        assert span.timestamp_ms.is_a? Integer
 
         # Note: should this go in recommended attributes?
         # It is recommended from the point of view of the
         # Trace API but at least some Telemetry SDK implementations
         # do require it on span creation.
-        assert_equal "Name", span.contents[:attributes][:name]
-        assert span.contents[:attributes][:name].is_a? String
+        assert_equal "Name", span.name
+        assert span.name.is_a? String
       end
 
       def test_recommended_attributes
         span = Span.new("Name",
-                        duration: 123456,
+                        duration_ms: 123456,
                         parent_id: "c617c2813a222a34",
                         service_name: "My Service")
 
-        expected_attributes = {
-          :name=>"Name",
-          :duration=>123456,
-          :parent_id=>"c617c2813a222a34",
-          :service_name=>"My Service"
-        }
+        assert_equal 123456, span.duration_ms
+        assert span.duration_ms.is_a? Integer
 
-        assert_equal expected_attributes, span.contents[:attributes]
+        assert_equal "c617c2813a222a34", span.parent_id
+        assert span.parent_id.is_a? String
 
-        assert_equal 123456, span.contents[:attributes][:duration]
-        assert span.contents[:attributes][:duration].is_a? Integer
-
-        assert_equal "c617c2813a222a34", span.contents[:attributes][:parent_id]
-        assert span.contents[:attributes][:parent_id].is_a? String
-
-        assert_equal "My Service", span.contents[:attributes][:service_name]
-        assert span.contents[:attributes][:service_name].is_a? String
+        assert_equal "My Service", span.service_name
+        assert span.service_name.is_a? String
       end
 
       def test_custom_attributes
         custom_attributes = {
           :'user.email' => "me@newr.com",
-          :something  => "somethingelse"
+          :custom_key   => "custom_value"
         }
 
         span = Span.new("Name", custom_attributes: custom_attributes)
 
-        expected_attributes = {
-          :name=>"Name",
-          :duration=>nil,
-          :parent_id=>nil,
-          :service_name=>nil,
-          :'user.email'=>"me@newr.com",
-          :something=>"somethingelse"
-        }
-
-        assert_equal expected_attributes, span.contents[:attributes]
+        assert_equal custom_attributes, span.custom_attributes
       end
 
       def test_finish
-        start_time = Util.time_to_ms
-        span = Span.new("Name", timestamp: start_time)
+        start_time_ms = Util.time_to_ms
+        span = Span.new("Name", timestamp_ms: start_time_ms)
 
-        # Mock a span that took 3 seconds
-        end_time = start_time + 3000
-        span.finish(end_time_ms: end_time)
+        end_time_ms = start_time_ms + 1000
+        span.finish(end_time_ms: end_time_ms)
 
-        assert_equal 3000, span.contents[:attributes][:duration]
+        assert_equal 1000, span.duration_ms
+      end
+
+      def test_to_json
+        id = Util.generate_guid(8)
+        trace_id = Util.generate_guid(16)
+        timestamp_ms = Util.time_to_ms
+
+        duration_ms = 1000
+        end_time_ms = timestamp_ms + 1000
+        custom_attributes = { :custom_key => "custom_value" }
+
+        span = Span.new("Name",
+                        id: id,
+                        trace_id: trace_id,
+                        timestamp_ms: timestamp_ms,
+                        parent_id: "c617c2813a222a34",
+                        service_name: "My Service",
+                        custom_attributes: custom_attributes)
+
+        span.finish(end_time_ms: end_time_ms)
+
+        expected_data = {
+          :id => id,
+          :'trace.id' => trace_id,
+          :timestamp  => timestamp_ms,
+          :attributes => {
+            :'duration.ms'  => duration_ms,
+            :'parent.id'    => "c617c2813a222a34",
+            :'service.name' => "My Service",
+            :custom_key   => "custom_value"
+          }
+        }.to_json
+
+        assert_equal expected_data, span.to_json
       end
     end
   end
