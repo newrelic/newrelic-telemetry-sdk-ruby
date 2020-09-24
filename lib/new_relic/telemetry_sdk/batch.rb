df9308df82e33f85d8c4fe8a389a666a058895af
Rachel Klein <rklein@newrelic.com>
2020-09-24 16:48:55 -0700

    Add SpanBatch

---
+++ lib/new_relic/telemetry_sdk/batch.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+# frozen_string_literal: true
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/newrelic-telemetry-sdk-ruby/blob/main/LICENSE for complete details.
+
+module NewRelic
+  module TelemetrySdk
+    class Batch
+      attr_accessor :items
+
+      def initialize common_attributes=nil
+        @items = []
+        @common_attributes = common_attributes
+        @lock = Mutex.new
+      end
+
+      # Items recorded into a batch must have a to_h method for transformation
+      def record item
+        @lock.synchronize { @items << item }
+      end
+
+      def flush
+        data = nil
+        @lock.synchronize do
+          data = @items.map(&:to_h)
+          @items = []
+        end
+        return data, @common_attributes
+      end
+    end
+  end
+end
---
+++ test/new_relic/telemetry_sdk/batch_test.rb
@@ -0,0 +1,44 @@
+# encoding: utf-8
+# frozen_string_literal: true
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/newrelic-telemetry-sdk-ruby/blob/main/LICENSE for complete details.
+
+require File.expand_path(File.join(File.dirname(__FILE__),'../..','test_helper'))
+
+require 'new_relic/telemetry_sdk/batch'
+require 'new_relic/telemetry_sdk/span'
+
+module NewRelic
+  module TelemetrySdk
+    class BatchTest < Minitest::Test
+      def setup
+        @batch = Batch.new
+      end
+
+      def test_record
+        span = Span.new
+        @batch.record span
+
+        assert_equal 1, @batch.items.length
+      end
+
+      def test_flush
+        spans = Span.new
+        @batch.record span
+
+        data, _ = @batch.flush
+
+        assert_equal 1, data.length
+      end
+
+      def test_common_attributes
+        common_attributes = { :foo => "bar" }
+        @batch_with_common_attributes = Batch.new common_attributes
+
+        _, actual = @batch_with_common_attributes.flush
+
+        assert_equal common_attributes, actual
+      end
+    end
+  end
+end
