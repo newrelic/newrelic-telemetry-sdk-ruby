6dcb8a5c9fd8f05a47e91edd66782b30297660fd
Rachel Klein <rklein@newrelic.com>
2020-09-28 15:57:48 -0700

    Add capacity for buffer

--- lib/new_relic/telemetry_sdk/buffer.rb
+++ lib/new_relic/telemetry_sdk/buffer.rb
@@ -9,15 +9,22 @@ module NewRelic
       attr_reader :items
       attr_accessor :common_attributes
 
+      CAPACITY = 2000
+
       def initialize common_attributes=nil
         @items = []
         @common_attributes = common_attributes
         @lock = Mutex.new
+        @capacity = capacity
       end
 
       # Items recorded into the buffer must have a to_h method for transformation
       def record item
-        @lock.synchronize { @items << item }
+        @lock.synchronize do
+          if size < capacity
+            @items << item
+          end
+        end
       end
 
       def flush
@@ -30,6 +37,14 @@ module NewRelic
       end
 
       alias_method :to_h, :flush
+
+      def size
+        @items.length
+      end
+
+      def capacity
+        CAPACITY
+      end
     end
   end
 end
