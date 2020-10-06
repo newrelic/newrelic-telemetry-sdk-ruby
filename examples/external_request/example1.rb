a70948d41652a9820a60796888d5d9bdf73cc601
Michael Lang <mlang@newrelic.com>
2020-10-06 16:42:41 -0400

    fixed example1

--- examples/external_request/example1.rb
+++ examples/external_request/example1.rb
@@ -2,6 +2,8 @@ require 'net/http'
 require 'bundler'
 Bundler.require
 
+require "new_relic/telemetry_sdk"
+
 unless ENV["API_KEY"]
   raise "No License key supplied.  Export API_KEY environment variable!" 
 end
@@ -21,20 +23,19 @@ def record_external_request
   finish_time = Time.now
   duration = finish_time - start_time
 
-  raise response.inspect
   custom_attributes = {
-    "name": response.path,
+    "name": "Net::HTTP#get",
     "http.method": "GET",
-    "http.status": response.status,
+    "http.status": response.code,
     "size": response.body.bytesize,
   }
 
   span = NewRelic::TelemetrySdk::Span.new(
     id: random_id(8),
     trace_id: random_id(16),
-    start_time_ms: (start_time * 1000).to_i,
+    start_time_ms: (start_time.to_i * 1000),
     duration_ms: (duration * 1000).to_i,
-    name: "get_#{response.path}",
+    name: "Net::HTTP#get",
     custom_attributes: custom_attributes
   )
   span_client.report span
@@ -45,10 +46,12 @@ end
 def get_page url
   uri = URI::parse(url)
   response = record_external_request do
-    Net::HTTP.get_response(uri)
+    conn = Net::HTTP.new uri.host, uri.port
+    conn.use_ssl = true
+    conn.request_get("/")
   end
   puts "Fetched: #{uri}"
-  puts "Status: #{response.status}"
+  puts "Status: #{response.code}"
   puts "Size: #{response.body.bytesize}"
 end
 
