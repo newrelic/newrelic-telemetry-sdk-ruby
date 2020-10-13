4a1afe7d84ca9a950e6afdda1a492ecf8f5bf185
Tanna McClure <tmcclure@newrelic.com>
2020-10-13 16:58:01 -0700

    added clarifying comments on tests

--- test/new_relic/telemetry_sdk/clients/client_test.rb
+++ test/new_relic/telemetry_sdk/clients/client_test.rb
@@ -148,6 +148,7 @@ module NewRelic
         @client.expects(:backoff_strategy).then.returns(0).once
         @client.instance_variable_set(:@max_retries, 5)
         @client.instance_variable_set(:@connection_attempts, 4)
+        # Retry raises an exception, so we want the exception raised here
         assert_raises NewRelic::TelemetrySdk::RetriableServerResponseException do 
           @client.log_and_retry_with_backoff(stub_response(413), [mock])
         end
@@ -156,6 +157,7 @@ module NewRelic
       def test_reaching_max_attempts_stops_retrying
         @client.instance_variable_set(:@max_retries, 5)
         @client.instance_variable_set(:@connection_attempts, 5)
+        # Retrying raises an exception, so we want to make sure there is no exception raised here
         @client.log_and_retry_with_backoff(stub_response(413), [mock])
       end
 
