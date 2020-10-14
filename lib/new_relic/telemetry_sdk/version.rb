603143b9473413a6e056cb8bdc022c36e5467105
Michael Lang <mlang@newrelic.com>
2020-10-14 11:17:33 -0400

    implemented the User-Agent header

--- lib/new_relic/telemetry_sdk/version.rb
+++ lib/new_relic/telemetry_sdk/version.rb
@@ -1,3 +1,8 @@
+# encoding: utf-8
+# frozen_string_literal: true
+# This file is distributed under New Relic's license terms.
+# See https://github.com/newrelic/newrelic-ruby-agent/blob/main/LICENSE for complete details.
+
 module NewRelic
   module TelemetrySdk
     VERSION = "0.1.0"
